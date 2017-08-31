e54904b0c1efa6498582b3012bf70895f1cb62e8
Nate Richardson <hello@naterichardson.com>
2017-08-30 23:53:41 -0400

    linting

--- xkcd.js
+++ xkcd.js
@@ -8,12 +8,14 @@ module.exports = function (Kirbi) {
 			usage: '[comic number]',
 			description: 'Displays a given XKCD comic number (or the latest if nothing specified',
 			process: (msg, suffix, isEdit, cb) => {
-				var url = "http://xkcd.com/";
-				if (suffix != "") url += `${suffix}/`;
+				let url = 'http://xkcd.com/';
+				if (suffix !== '') {
+					url += `${suffix}/`;
+				}
 				url += 'info.0.json';
-				require('request')(url, function (err, res, body) {
+				require('request')(url, (err, res, body) => {
 					try {
-						var comic = JSON.parse(body);
+						let comic = JSON.parse(body);
 						cb({embed: {
 							color: Kirbi.Config.discord.defaultEmbedColor,
 							title: `XKCD ${comic.num} ${comic.title}`,
@@ -33,9 +35,9 @@ module.exports = function (Kirbi) {
 		highnoon: {
 			process: (msg, suffix, isEdit, cb) => {
 				require('request')({
-					uri: "http://imgs.xkcd.com/comics/now.png",
+					uri: 'http://imgs.xkcd.com/comics/now.png',
 					followAllRedirects: true
-				}, (err, resp, body) => cb({embed: { image: {url: resp.request.uri.href}}}, msg));
+				}, (err, resp) => cb({embed: {image: {url: resp.request.uri.href}}}, msg));
 			}
 		}
 	};
